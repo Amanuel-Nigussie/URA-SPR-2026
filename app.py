4105bdbe48d9e5ea2015238a3946ac899716fcce
Amanuel-Nigussie <160407550+Amanuel-Nigussie@users.noreply.github.com>
2026-03-16 13:16:48 +0000

    cos

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from analysis import process_folds
+from lev_analysis import process_folds
 
 
 # -----------------------------------------------------------
